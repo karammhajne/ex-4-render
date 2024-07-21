2be58a9e37952eef22feef5e7083f7264bad72f6
karammhajne <165606532+karammhajne@users.noreply.github.com>
2024-07-21 23:37:51 +0300

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,22 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  logging: console.log,  // Enable logging to see SQL queries
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
+
+export default sequelize;
