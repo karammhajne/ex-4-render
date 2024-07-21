ae433b8987fb837cddc3a02448908df276e9da7f
karammhajne <165606532+karammhajne@users.noreply.github.com>
2024-07-21 23:33:37 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,10 @@
 import express from 'express';
-import { registerUser, loginUser, getUserPreferences, setUserPreferences, registerUsers } from '../controllers/user.js';
-import { calculateVacation } from '../controllers/vacation.js';
+import { registerUser, loginUser } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
-router.post('/registerBulk', registerUsers);
 router.post('/login', loginUser);
-router.get('/preferences', getUserPreferences);
-router.post('/preferences', setUserPreferences);
-router.get('/calculate', calculateVacation);
 
 export default router;
+
